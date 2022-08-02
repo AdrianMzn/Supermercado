0ae5a9d4aea270e2a76d252f369e0021e1c32d80
dorusbo <dorusbo@alumni.uv.es>
2022-08-02 21:22:23 +0200

    componentes y pagina central
    3 componentes para grupos de productos y la pagina de navegación entre ellos central

--- Supermercado/src/app/app.module.ts
+++ Supermercado/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente/componente1/componente1.component';
+import { Componente2Component } from './componente/componente2/componente2.component';
+import { Componente3Component } from './componente/componente3/componente3.component';
+
+const misRutas: Routes = [
+  {path: 'c1', component: Componente1Component},
+  {path: 'c2', component: Componente2Component},
+  {path: 'c3', component: Componente3Component}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(misRutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
