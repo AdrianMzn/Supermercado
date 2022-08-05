8e27460bba4349df96089fbc153812e4cfa90c58
sergeisg <sergeisobera@gmail.com>
2022-08-05 22:04:30 +0200

    añadir precio total del carrito

--- Supermercado/src/app/componentes/carrito/carrito.component.ts
+++ Supermercado/src/app/componentes/carrito/carrito.component.ts
@@ -10,25 +10,43 @@ import { CartService } from 'src/app/servicios/carrito.service';
 export class CarritoComponent implements OnInit {
 
   addedProducts: Producto[] = []
+  totalPrice: number = 0
 
   constructor(private cartService: CartService) { 
   }
   
   ngOnInit(): void {
-    this.cartService.getAll().subscribe((products) => (this.addedProducts = products))
+    this.cartService.getAll().subscribe((products) => {
+      this.addedProducts = products
+      this.totalPrice = Math.round(this.addedProducts.reduce((acc, prod) => (acc + prod.precio)*prod.cantidad, 0)*100)/100
+    })
   }
-
   removeProduct(product: Producto){
     console.log(product)
-    this.cartService.delete(product).subscribe((products) => (this.addedProducts = this.addedProducts.filter((prod) => (prod.id !== product.id))))
+    this.cartService
+      .delete(product)
+      .subscribe((products) => {
+        this.addedProducts = this.addedProducts.filter((prod) => (prod.id !== product.id))
+        this.totalPrice = Math.round(this.addedProducts.reduce((acc, prod) => (acc + prod.precio)*prod.cantidad, 0)*100)/100
+      })
   }
 
   increaseQuantity(product:Producto){
-    this.cartService.plusOne(product).subscribe((product) => (this.addedProducts = this.addedProducts.map((prod) => (prod.id === product.id ? product : prod))))
+    this.cartService
+    .plusOne(product)
+    .subscribe((product) => {
+      this.addedProducts = this.addedProducts.map((prod) => (prod.id === product.id ? product : prod))
+      this.totalPrice = Math.round(this.addedProducts.reduce((acc, prod) => (acc + prod.precio)*prod.cantidad, 0)*100)/100
+    })
   }
 
   decreaseQuantity(product:Producto){
-    this.cartService.minusOne(product).subscribe((product) => (this.addedProducts = this.addedProducts.map((prod) => (prod.id === product.id ? product : prod))))
+    this.cartService
+    .minusOne(product)
+    .subscribe((product) => {
+      this.addedProducts = this.addedProducts.map((prod) => (prod.id === product.id ? product : prod))
+      this.totalPrice = Math.round(this.addedProducts.reduce((acc, prod) => (acc + prod.precio)*prod.cantidad, 0)*100)/100
+    })
   }
 
 }
