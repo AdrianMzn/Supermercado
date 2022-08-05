ecb6aa65e7ef1aaef00110da6f8f7830ea2f7ef5
sergeisg <sergeisobera@gmail.com>
2022-08-05 19:45:19 +0200

    añadir funcion getAll al componente producto y pintar los productos en el html

--- Supermercado/src/app/componentes/producto/producto.component.ts
+++ Supermercado/src/app/componentes/producto/producto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../../models/producto.model';
+import { ProductsService } from 'src/app/servicios/productos.service';
 
 @Component({
   selector: 'app-producto',
@@ -9,32 +10,10 @@ import { Producto } from '../../models/producto.model';
 export class ProductoComponent implements OnInit {
   productos: Producto[] = []
 
-  constructor() { }
+  constructor(private prodService: ProductsService) { }
 
   ngOnInit(): void {
-/*     this.generarProductos()
- */  }
-
-/*   generarProductos() {
-    let producto1: Producto = {
-      id: 1,
-      seccion: 'lacteos',
-      nombre: 'leche',
-      precio: 1.30,
-      descripcion: 'loren ipsum',
-      imagen: 'windowWhen.gertwtn',
-    }
-    this.productos.push(producto1)
-
-    let producto2: Producto = {
-      id: 2,
-      seccion: 'verduras',
-      nombre: 'zanahoria',
-      precio: 0.80,
-      descripcion: 'loren ipsum',
-      imagen: 'windowWhen.gertwtn',
-    }
-    this.productos.push(producto2)
+    this.prodService.getAll().subscribe((productos) => (this.productos = productos))
   }
- */
+
 }
