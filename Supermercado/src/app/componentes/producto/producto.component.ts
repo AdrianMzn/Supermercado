6947d14af76b035b2cf2aa6b8d6751ad8a0b3ca6
sergeisg <sergeisobera@gmail.com>
2022-08-05 19:49:42 +0200

    añadir boton para añadir productos al carrito

--- Supermercado/src/app/componentes/producto/producto.component.ts
+++ Supermercado/src/app/componentes/producto/producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../../models/producto.model';
 import { ProductsService } from 'src/app/servicios/productos.service';
+import { CartService } from 'src/app/servicios/carrito.service';
 
 @Component({
   selector: 'app-producto',
@@ -9,11 +10,16 @@ import { ProductsService } from 'src/app/servicios/productos.service';
 })
 export class ProductoComponent implements OnInit {
   productos: Producto[] = []
+  productosCarrito: Producto[] = []
 
-  constructor(private prodService: ProductsService) { }
+  constructor(private prodService: ProductsService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.prodService.getAll().subscribe((productos) => (this.productos = productos))
+  }
+
+  addProducto(producto: Producto){
+    this.cartService.add(producto).subscribe((prod) => (this.productosCarrito.push(prod)))
   }
 
 }
